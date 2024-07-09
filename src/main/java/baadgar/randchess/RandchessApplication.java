5c7d4c94e9d722f9846653a1b7608d8c59574fe1
SimpleBadger <wojciechrzucidlo22@gmail.com>
2024-07-09 11:59:28 +0200

    Base commit

---
+++ src/main/java/baadgar/randchess/RandchessApplication.java
@@ -0,0 +1,13 @@
+package baadgar.randchess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandchessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandchessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/baadgar/randchess/RandchessApplicationTests.java
@@ -0,0 +1,13 @@
+package baadgar.randchess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandchessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
